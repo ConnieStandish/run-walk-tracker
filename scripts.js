dc7fa33d01c10c90e2966ef191da98d763eefbe6
Connie Standish <conniestand@gmail.com>
2024-11-09 20:42:15 -0500

    Changed background color of body when map opens

--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ const options = {
 
 function start() {
     if (navigator.geolocation) {
-        background.style.backgroundColor = "rgba(0, 0, 0, 0.5)"
+        background.style.backgroundColor = "rgb(45, 41, 40)"
         modal.style.visibility = "visible";
         background.style.visibility = "hidden"
         tracking = true;
